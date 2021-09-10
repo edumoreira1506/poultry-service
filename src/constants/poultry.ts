1616239fc680f604954feb21232205598f936b05
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-09 23:18:10 -0300

    Add validations

--- src/constants/poultry.ts
+++ src/constants/poultry.ts
@@ -3,3 +3,33 @@ export const MAXIMUM_CHARACTERS_NAME = 200
 
 export const MINIMUM_CHARACTERS_DESCRIPTION = 3
 export const MAXIMUM_CHARACTERS_DESCRIPTION = 250
+
+export const AVAILABLE_PROVINCES = [
+  'AC',
+  'AL',
+  'AP',
+  'AM',
+  'BA',
+  'CE',
+  'DF',
+  'ES',
+  'GO',
+  'MA',
+  'MT',
+  'MS',
+  'MG',
+  'PA',
+  'PB',
+  'PR',
+  'PE',
+  'PI',
+  'RJ',
+  'RN',
+  'RS',
+  'RO',
+  'RR',
+  'SC',
+  'SP',
+  'SE',
+  'TO',
+]
--- src/schemas/PoultrySchemas.ts
+++ src/schemas/PoultrySchemas.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 
 import i18n from '@Configs/i18n'
-import { MAXIMUM_CHARACTERS_DESCRIPTION, MAXIMUM_CHARACTERS_NAME, MINIMUM_CHARACTERS_DESCRIPTION, MINIMUM_CHARACTERS_NAME } from '@Constants/poultry'
+import { AVAILABLE_PROVINCES, MAXIMUM_CHARACTERS_DESCRIPTION, MAXIMUM_CHARACTERS_NAME, MINIMUM_CHARACTERS_DESCRIPTION, MINIMUM_CHARACTERS_NAME } from '@Constants/poultry'
 
 export const storePoultrySchema = Joi.object({
   name: Joi.string().required().min(MINIMUM_CHARACTERS_NAME).max(MAXIMUM_CHARACTERS_NAME).messages({
@@ -17,9 +17,10 @@ export const storePoultrySchema = Joi.object({
       'string.empty': i18n.__('empty-field', { field: i18n.__('poultry.fields.address.city') }),
       'any.required': i18n.__('required-field', { field: i18n.__('poultry.fields.address.city') })
     }),
-    province: Joi.string().required().messages({
+    province: Joi.string().required().valid(...AVAILABLE_PROVINCES) .messages({
       'string.empty': i18n.__('empty-field', { field: i18n.__('poultry.fields.address.province') }),
-      'any.required': i18n.__('required-field', { field: i18n.__('poultry.fields.address.province') })
+      'any.required': i18n.__('required-field', { field: i18n.__('poultry.fields.address.province') }),
+      'any.only': i18n.__('poultry.errors.invalid-address-province')
     }),
     street: Joi.string().required().messages({
       'string.empty': i18n.__('empty-field', { field: i18n.__('poultry.fields.address.street') }),
@@ -29,9 +30,10 @@ export const storePoultrySchema = Joi.object({
       'string.empty': i18n.__('empty-field', { field: i18n.__('poultry.fields.address.zipcode') }),
       'any.required': i18n.__('required-field', { field: i18n.__('poultry.fields.address.zipcode') })
     }),
-    number: Joi.number().required().messages({
-      'string.empty': i18n.__('empty-field', { field: i18n.__('poultry.fields.address.number') }),
-      'any.required': i18n.__('required-field', { field: i18n.__('poultry.fields.address.number') })
+    number: Joi.number().required().min(1).messages({
+      'number.empty': i18n.__('empty-field', { field: i18n.__('poultry.fields.address.number') }),
+      'any.required': i18n.__('required-field', { field: i18n.__('poultry.fields.address.number') }),
+      'number.min': i18n.__('poultry.errors.invalid-address-number')
     }),
   })
 }).options({ abortEarly: false })
