abc3fdeb386286f5603b20fd67b7efc2f0886605
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-17 19:32:36 -0300

    Export poultries docs

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,9 +1,11 @@
 import breederDocs from './breeder'
 import breederUserDocs from './breederUser'
 import breederImageDocs from './breederImage'
+import poultriesDocs from './poultry'
 
 export default {
   ...breederDocs,
   ...breederUserDocs,
   ...breederImageDocs,
+  ...poultriesDocs
 }
