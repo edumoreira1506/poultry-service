fa576cd8cd43e1f2bb287540214c6f8297d6aa9f
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-19 21:05:28 -0300

    Add docs

---
+++ src/docs/breederContact.ts
@@ -0,0 +1,17 @@
+import createDoc from '@cig-platform/docs/build/docs/createDoc'
+
+import { storeBreederContactSchema } from '@Schemas/BreederContactSchema'
+
+const breederContactDocs = {
+  ...createDoc('/breeders/{breederId}/contacts', ['Contacts'], [
+    {
+      method: 'post',
+      title: 'Store contact',
+      objectSchema: storeBreederContactSchema,
+    }
+  ], {
+    pathVariables: [{ type: 'string', name: 'breederId' }]
+  })
+}
+
+export default breederContactDocs
--- src/docs/index.ts
+++ src/docs/index.ts
@@ -2,10 +2,12 @@ import breederDocs from './breeder'
 import breederUserDocs from './breederUser'
 import breederImageDocs from './breederImage'
 import poultriesDocs from './poultry'
+import breederContactDocs from './breederContact'
 
 export default {
   ...breederDocs,
   ...breederUserDocs,
   ...breederImageDocs,
-  ...poultriesDocs
+  ...poultriesDocs,
+  ...breederContactDocs
 }
