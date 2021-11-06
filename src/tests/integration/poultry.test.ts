a8fa97e66d70e7419fc8ac0cd062370f82ae6332
Eduardo Moreira <contato@eduardoem.com.br>
2021-11-06 19:40:04 -0300

    Add tests

--- src/tests/integration/poultry.test.ts
+++ src/tests/integration/poultry.test.ts
@@ -6,6 +6,7 @@ import App from '@Configs/server'
 import i18n from '@Configs/i18n'
 import PoultryController from '@Controllers/PoultryController'
 import PoultryGenderEnum from '@Enums/PoultryGenderEnum'
+import PoultryGenderCategoryEnum from '@Enums/PoultryGenderCategoryEnum'
 
 jest.mock('typeorm', () => ({
   createConnection: jest.fn().mockResolvedValue({}),
@@ -39,7 +40,8 @@ describe('Poultry actions', () => {
         colors: poultry.colors,
         videos: poultry.videos,
         type: poultry.type,
-        gender: PoultryGenderEnum.Female
+        gender: PoultryGenderEnum.Female,
+        genderCategory: PoultryGenderCategoryEnum.Matrix
       })
 
       expect(response.statusCode).toBe(200)
@@ -53,6 +55,65 @@ describe('Poultry actions', () => {
         videos: poultry.videos,
         type: poultry.type
       }))
+    })
+
+    it('is an invalid poultry when does not send gender category', async () => {
+      const mockSave = jest.fn()
+      const poultry = poultryFactory({ colors: {} })
+      const breeder = breederFactory()
+
+      jest.spyOn(typeorm, 'getCustomRepository').mockReturnValue({
+        save: mockSave,
+        findById: jest.fn().mockResolvedValue(breeder),
+      })
+
+      const response = await request(App).post(`/v1/breeders/${breeder.id}/poultries`).send({
+        birthDate: poultry.birthDate,
+        colors: poultry.colors,
+        videos: poultry.videos,
+        type: poultry.type,
+        gender: PoultryGenderEnum.Female,
+      })
+
+      expect(response.statusCode).toBe(400)
+      expect(response.body).toMatchObject({
+        ok: false,
+        error: {
+          name: 'ValidationError',
+          message: i18n.__('required-field', { field: i18n.__('poultry.fields.gender-category') })
+        }
+      })
+      expect(mockSave).not.toHaveBeenCalled()
+    })
+
+    it('is an invalid poultry when gender and gender category are incompatible', async () => {
+      const mockSave = jest.fn()
+      const poultry = poultryFactory({ colors: {} })
+      const breeder = breederFactory()
+
+      jest.spyOn(typeorm, 'getCustomRepository').mockReturnValue({
+        save: mockSave,
+        findById: jest.fn().mockResolvedValue(breeder),
+      })
+
+      const response = await request(App).post(`/v1/breeders/${breeder.id}/poultries`).send({
+        birthDate: poultry.birthDate,
+        colors: poultry.colors,
+        videos: poultry.videos,
+        type: poultry.type,
+        gender: PoultryGenderEnum.Female,
+        genderCategory: PoultryGenderCategoryEnum.MaleChicken
+      })
+
+      expect(response.statusCode).toBe(400)
+      expect(response.body).toMatchObject({
+        ok: false,
+        error: {
+          name: 'ValidationError',
+          message: i18n.__('poultry.errors.invalid-gender-category')
+        }
+      })
+      expect(mockSave).not.toHaveBeenCalled()
     })
    
     it('is an invalid poultry when register is dupliacted', async () => {
@@ -73,6 +134,7 @@ describe('Poultry actions', () => {
         videos: poultry.videos,
         type: poultry.type,
         gender: PoultryGenderEnum.Female,
+        genderCategory: PoultryGenderCategoryEnum.Matrix,
         register
       })
 
