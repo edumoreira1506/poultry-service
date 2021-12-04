fd6639d727ec39352b594f90acce203f6913c2de
Eduardo Moreira <contato@eduardoem.com.br>
2021-12-04 19:26:41 -0300

    Add remove advertising

--- src/constants/register.ts
+++ src/constants/register.ts
@@ -3,4 +3,4 @@ import RegisterTypeEnum from '@Enums/RegisterTypeEnum'
 export const MINIMUM_CHARACTERS_DESCRIPTION = 3
 export const MAXIMUM_CHARACTERS_DESCRIPTION = 200
 
-export const TYPES_WITH_METADATA = [RegisterTypeEnum.Advertising, RegisterTypeEnum.Advertising]
+export const TYPES_WITH_METADATA = [RegisterTypeEnum.Advertising, RegisterTypeEnum.RemoveAdvertising]
