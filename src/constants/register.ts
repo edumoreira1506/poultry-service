91716cf6c4be8ab5b02deefa609a729afac7ba26
Eduardo Moreira <contato@eduardoem.com.br>
2021-12-09 18:01:05 -0300

    Add transfer register type

--- src/constants/register.ts
+++ src/constants/register.ts
@@ -3,4 +3,8 @@ import RegisterTypeEnum from '@Enums/RegisterTypeEnum'
 export const MINIMUM_CHARACTERS_DESCRIPTION = 3
 export const MAXIMUM_CHARACTERS_DESCRIPTION = 200
 
-export const TYPES_WITH_METADATA = [RegisterTypeEnum.Advertising, RegisterTypeEnum.RemoveAdvertising]
+export const TYPES_WITH_METADATA = [
+  RegisterTypeEnum.Advertising,
+  RegisterTypeEnum.RemoveAdvertising,
+  RegisterTypeEnum.Transfer
+]
--- src/enums/RegisterTypeEnum.ts
+++ src/enums/RegisterTypeEnum.ts
@@ -4,7 +4,8 @@ enum RegisterTypeEnum {
   Weighing = 'PESAGEM',
   Vaccination = 'VACINAÇÃO',
   Advertising = 'ANÚNCIO',
-  RemoveAdvertising = 'REMOÇÃO DE ANÚNCIO'
+  RemoveAdvertising = 'REMOÇÃO DE ANÚNCIO',
+  Transfer = 'TRANSFERÊNCIA'
 }
 
 export default RegisterTypeEnum
