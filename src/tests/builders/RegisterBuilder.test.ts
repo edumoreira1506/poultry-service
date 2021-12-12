b33221e75cd8c5fbdf8aabb60de298178b5b44c7
Eduardo Moreira <contato@eduardoem.com.br>
2021-12-12 14:50:58 -0300

    Add tests

---
+++ src/tests/builders/RegisterBuilder.test.ts
@@ -0,0 +1,40 @@
+import { poultryRegisterFactory } from '@cig-platform/factories'
+
+import RegisterBuilder from '@Builders/RegisterBuilder'
+import { TYPES_WITH_METADATA } from '@Constants/register'
+import RegisterTypeEnum from '@Enums/RegisterTypeEnum'
+import i18n from '@Configs/i18n'
+
+describe('RegisterBuilder', () => {
+  describe('.build', () => {
+    it('a valid register', () => {
+      const register = poultryRegisterFactory()
+      const registerBuilder = new RegisterBuilder()
+        .setDate(register.date.toString())
+        .setDescription(register.description)
+        .setType(register.type)
+        .setMetadata({})
+
+      expect(registerBuilder.build()).toMatchObject({
+        description: register.description,
+        type: register.type,
+      })
+    })
+
+    const typesWithouthMetadata = Object.values(RegisterTypeEnum).filter(type => !TYPES_WITH_METADATA.includes(type))
+
+    typesWithouthMetadata.forEach((type) => {
+      it(`throwns an error when has metadata and the type is ${type}`, () => {
+        const register = poultryRegisterFactory({ type })
+        const metadata = { a: 'oi' }
+        const registerBuilder = new RegisterBuilder()
+          .setDate(register.date.toString())
+          .setDescription(register.description)
+          .setType(register.type)
+          .setMetadata(metadata)
+  
+        expect(registerBuilder.build).toThrow(i18n.__('register.errors.invalid-metadata'))
+      })
+    })
+  })
+})
