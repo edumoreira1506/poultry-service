4d13a0df5b305e828814b5990efc213c8c4408d4
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-24 21:00:08 -0300

    Chicken 299 (#65)
    * Add poultry gender category validation
    
    * Add tests

---
+++ src/constants/time.ts
@@ -0,0 +1,6 @@
+export const SECOND = 1000
+export const MINUTE = 60 * SECOND
+export const HOUR = 60 * MINUTE
+export const DAY = 24 * HOUR
+export const MONTH = 30 * DAY
+export const YEAR = DAY * 365
