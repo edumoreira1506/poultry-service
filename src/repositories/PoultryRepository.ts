c924e791ecd113593edd481a74b9238dd2e74c0d
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-03 20:52:44 -0300

    Chicken 536 (#75)
    * Add pages to search poultries route
    
    * Add function createFilters
    
    * Fix page logic

--- src/repositories/PoultryRepository.ts
+++ src/repositories/PoultryRepository.ts
@@ -3,8 +3,58 @@ import { BaseRepository } from '@cig-platform/core'
 
 import Poultry from '@Entities/PoultryEntity'
 
+const ITEMS_PER_PAGE = 30
+
 @EntityRepository(Poultry)
 export default class PoultryRepository extends BaseRepository<Poultry> {
+  static createFilters({
+    gender,
+    genderCategory,
+    poultryIds = [],
+    forSale,
+    type,
+    crest,
+    dewlap,
+    tail,
+    description,
+    name,
+    prices,
+  }: {
+    gender?: string[];
+    genderCategory?: string[];
+    poultryIds?: string[];
+    forSale?: boolean;
+    type?: string[];
+    crest?: string[];
+    dewlap?: string[];
+    tail?: string[];
+    description?: string;
+    name?: string;
+    prices?: { min?: number; max?: number };
+  } = {}) {
+    const commonQueryParams = {
+      active: true,
+      ...(gender?.length ? { gender: In(gender) } : {}),
+      ...(genderCategory?.length ? { genderCategory: In(genderCategory) } : {}),
+      ...(poultryIds.length ? { id: In(poultryIds) } : {}),
+      ...(type?.length ? { type: In(type) } : {}),
+      ...(crest?.length ? { crest: In(crest) } : {}),
+      ...(dewlap?.length ? { dewlap: In(dewlap) } : {}),
+      ...(tail?.length ? { tail: In(tail) } : {}),
+      ...(typeof forSale === 'boolean' ? { forSale } : {}),
+      ...(typeof prices?.min === 'number' && typeof prices?.max === 'number' ? {
+        currentAdvertisingPrice: Between(prices.min, prices.max)
+      } : {})
+    }
+    const queryParams = [
+      (name ? { name: Like(`%${name}%`), ...commonQueryParams } : undefined),
+      (description ? { description: Like(`%${description}%`), ...commonQueryParams } : undefined),
+      (!name && !description ? commonQueryParams : undefined)
+    ].filter(Boolean)
+
+    return queryParams
+  }
+
   search({
     gender,
     genderCategory,
@@ -34,25 +84,19 @@ export default class PoultryRepository extends BaseRepository<Poultry> {
     sort?: string;
     page?: number;
   } = {}) {
-    const commonQueryParams = {
-      active: true,
-      ...(gender?.length ? { gender: In(gender) } : {}),
-      ...(genderCategory?.length ? { genderCategory: In(genderCategory) } : {}),
-      ...(poultryIds.length ? { id: In(poultryIds) } : {}),
-      ...(type?.length ? { type: In(type) } : {}),
-      ...(crest?.length ? { crest: In(crest) } : {}),
-      ...(dewlap?.length ? { dewlap: In(dewlap) } : {}),
-      ...(tail?.length ? { tail: In(tail) } : {}),
-      ...(typeof forSale === 'boolean' ? { forSale } : {}),
-      ...(typeof prices?.min === 'number' && typeof prices?.max === 'number' ? {
-        currentAdvertisingPrice: Between(prices.min, prices.max)
-      } : {})
-    }
-    const queryParams = [
-      (name ? { name: Like(`%${name}%`), ...commonQueryParams } : undefined),
-      (description ? { description: Like(`%${description}%`), ...commonQueryParams } : undefined),
-      (!name && !description ? commonQueryParams : undefined)
-    ].filter(Boolean)
+    const queryParams = PoultryRepository.createFilters({
+      gender,
+      genderCategory,
+      poultryIds,
+      forSale,
+      type,
+      crest,
+      dewlap,
+      tail,
+      description,
+      name,
+      prices,
+    })
 
     return this.find({
       where: queryParams,
@@ -61,9 +105,55 @@ export default class PoultryRepository extends BaseRepository<Poultry> {
         ...(sort === 'MAX_TO_MIN' ? { currentAdvertisingPrice: 'DESC' } : {}),
         ...(sort === 'MIN_TO_MAX' ? { currentAdvertisingPrice: 'ASC' } : {}),
       },
-      skip: page,
-      take: 30
+      skip: page * ITEMS_PER_PAGE,
+      take: ITEMS_PER_PAGE
     })
+  }
+
+  async countPages({
+    gender,
+    genderCategory,
+    poultryIds = [],
+    forSale,
+    type,
+    crest,
+    dewlap,
+    tail,
+    description,
+    name,
+    prices,
+  }: {
+    gender?: string[];
+    genderCategory?: string[];
+    poultryIds?: string[];
+    forSale?: boolean;
+    type?: string[];
+    crest?: string[];
+    dewlap?: string[];
+    tail?: string[];
+    description?: string;
+    name?: string;
+    prices?: { min?: number; max?: number };
+  } = {}) {
+    const queryParams = PoultryRepository.createFilters({
+      gender,
+      genderCategory,
+      poultryIds,
+      forSale,
+      type,
+      crest,
+      dewlap,
+      tail,
+      description,
+      name,
+      prices,
+    })
+
+    const poultriesAmount = await this.count({
+      where: queryParams
+    })
+
+    return Math.ceil(poultriesAmount / ITEMS_PER_PAGE)
   }
 
   findByBreeder(
